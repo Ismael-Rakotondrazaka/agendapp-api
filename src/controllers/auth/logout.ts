8e44e3be0ab6e6d543fa690d908cf85d3add3aad
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2022-11-04 07:21:16 +0300

    Add logout feature

---
+++ src/controllers/auth/logout.ts
@@ -0,0 +1,43 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Request, Response, NextFunction } from "express";
+import { BadRequestError, UnauthorizedError } from "../../utils/errors";
+import { User } from "../../models";
+import { Types } from "mongoose";
+import { IRefreshTokens } from "../../types";
+
+export default async function logout(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> {
+  try {
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) {
+      throw new BadRequestError("Property refresh token is required.");
+    }
+
+    const foundUser = await User.findOne({
+      "refreshTokens.token": refreshToken,
+    });
+
+    if (!foundUser) {
+      throw new UnauthorizedError(""); // ? why
+    }
+
+    const refreshTokenId = (
+      foundUser.refreshTokens.find(
+        (value) => value.token === refreshToken
+      ) as Types.ObjectId & IRefreshTokens
+    )?._id;
+
+    foundUser.refreshTokens.id(refreshTokenId)?.remove();
+
+    await foundUser.save();
+
+    return res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+}
